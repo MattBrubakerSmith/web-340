5f3bf0eaf2a344bc5e6cb222d246b0987d3639c7
MattBrubakerSmith <matthewbrubakersmith@gmail.com>
2018-08-06 11:59:14 -0500

    Added Exercise 2.3 and updated Exercise 2.2

--- week-2/routes/smith-exercise-2.3.js
+++ week-2/routes/smith-exercise-2.3.js
@@ -1,11 +1,11 @@
 /*
 ============================================
-; Title: Exercise 2.2
+; Title: Exercise 2.3
 ; Author: Professor Krasso
 ; Date: 6 August 2018
 ; Modified By: Matthew Smith
 ; Description: This program demonstrates
-; how to run Express/NodeJS on a local server.
+; how to use routes with Express/NodeJS.
 ;===========================================
 */
 
@@ -14,9 +14,21 @@ var Http = require("http");
 
 var app = Express();
 
+app.get("/", function(req, res) {
+    res.end("Welcome to the homepage!");
+});
+
+app.get("/about", function(req, res) {
+    res.end("Welcome to the about page!");
+});
+
+app.get("/contact", function(req, res) {
+    res.end("Welcome to the contact page!");
+});
+
 app.use(function(req, res) {
-    console.log("In comes a request to: " + req.url);
-    res.end("Hello World");
+    res.statusCode = 404;
+    res.end("404!");
 });
 
 Http.createServer(app).listen(8080);
