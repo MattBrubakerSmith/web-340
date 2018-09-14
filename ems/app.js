ed0b6e0fd66a703edc94b1371883e8b9553c3679
MattBrubakerSmith <matthewbrubakersmith@gmail.com>
2018-09-14 09:33:55 -0500

    Updated EMS with Exercise 8.3

--- ems/app.js
+++ ems/app.js
@@ -3,9 +3,69 @@ var http = require("http");
 var path = require("path");
 var logger = require("morgan");
 var helmet = require("helmet");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
 var mongoose = require("mongoose");
 var mongoDB = "mongodb://smaft:smaft1@ds145752.mlab.com:45752/ems";
 var Employee = require("./models/employee");
+
+// Express App
+var app = express();
+
+// Morgan Logger
+app.use(logger("short"));
+
+// Security
+var csrfProtection = csrf({ cookie: true });
+var parseForm = bodyParser.urlencoded({ extended: true });
+app.use(helmet.xssFilter());
+app.use(parseForm);
+app.use(cookieParser());
+app.use(csrfProtection);
+app.use(function(req, res, next) {
+    let token = req.csrfToken();
+    res.cookie("XSRF-TOKEN", token);
+    res.locals.csrfToken = token;
+    next();
+});
+
+// Static Files
+app.use(express.static(__dirname));
+
+// View Engine
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Express Routing
+app.get("/", function(req, res) {
+    res.render("index", {
+        title: "Home Page"
+    });
+});
+
+app.get("/list", function(req, res) {
+    res.render("list", {
+        title: "Employee List"
+    });
+});
+
+app.get("/new", function(req, res) {
+    res.render("new", {
+        title: "Add New Employee",
+        csrfToken: req.csrfToken()
+    });
+});
+
+app.post("/process", function(req, res) {
+    if (!req.body.txtName) {
+        res.status(400).send("Entries must have a name!");
+        return;
+    }
+
+    console.log(req.body.txtName);
+    res.redirect("/list");
+});
 
 // Mongo Connection 
 mongoose.connect(mongoDB, {
@@ -18,41 +78,6 @@ var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connected error: "));
 db.once("open", function() {
     console.log("Application connected to mLab MongoDB instance.");
-});
-
-// Express App
-var app = express();
-app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(logger("short"));
-app.use(helmet.xssFilter());
-
-app.use(express.static(__dirname));
-
-//Express Routing
-app.get("/", function(req, res) {
-    res.render("index", {
-        title: "Home Page",
-        message: "XSS Prevention Example"
-    });
-});
-
-app.get("/list", function(req, res) {
-    res.render("list", {
-        title: "Employee List"
-    });
-});
-
-app.get("/new", function(req, res) {
-    res.render("new", {
-        title: "Add Employee"
-    });
-});
-
-//Create Employee
-var employee = new Employee({
-    firstName: "Em",
-    lastName: "Ployee"
 });
 
 // HTTP Server
